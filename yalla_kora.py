7343865ff48c9899ac9b5ab841b32975d244579f
Mohamed Eleraqi <mohamed.8.eleraqi@gmail.com>
2024-12-19 20:05:57 +0200

    new && git push

--- yalla_kora.py
+++ yalla_kora.py
@@ -1,61 +1,89 @@
-import requests                          # pip install requests
-from bs4 import BeautifulSoup as Bs             # pip install beautifulsoup # pip install lxml
-import csv                                      
+"""Web Scraping Task That get the match's data
+    """
 
-# date=input("Enter The date eg. 'MM/dd/yyyy' : -> ")
-date="12/14/2024"
-page = requests.get(f"https://www.yallakora.com/match-center/?date={date}")
+import csv
+import requests  # pip install requests
+from bs4 import BeautifulSoup as Bs  # pip install beautifulsoup # pip install lxml
 
-def main(page):
-    src = page.content
-    soup=Bs(src,"lxml")
+date = input("Enter The date eg. 'MM/dd/yyyy' : -> ")
+# date="12/14/2024"
+page = requests.get(f"https://www.yallakora.com/match-center/?date={date}", timeout=20)
+
+
+def main(page_):
+    """_summary_
+
+    Args:
+        page_ (_type_): _description_
+    """
+    src = page_.content
+    soup = Bs(src, "lxml")
     matches_details = []
-    championShips = soup.find_all("div",{'class':'matchCard'} )  
-    # (str tag, dict filter) here we git the main div to championships 
-    # print (championShips)
-    
-    def get_match_info(championShip):
-        championShip_title = championShip.contents[1].find('h2').text.strip() # this is equal the one below.
+    champion_ships = soup.find_all("div", {"class": "matchCard"})
+    # (str tag, dict filter) here we git the main div to championships
+
+    def get_match_info(champion_ship):
+        champion_ship_title = (
+            champion_ship.contents[1].find("h2").text.strip()
+        )  # this is equal the one below.
         # championShip_title = championShip.find('h2').text.strip()
-        all_matches = championShip.contents[3].find_all("div",{'class':'liItem'})
+        all_matches = champion_ship.contents[3].find_all("div", {"class": "liItem"})
         number_of_matches = len(all_matches)
         for i in range(number_of_matches):
             # Get Teams Name
-            teamA=all_matches[i].find('div',{'class':'teamA'}).text.strip()
-            teamB=all_matches[i].find('div',{'class':'teamB'}).text.strip()
-            
+            team_a = all_matches[i].find("div", {"class": "teamA"}).text.strip()
+            team_b = all_matches[i].find("div", {"class": "teamB"}).text.strip()
+
             # Get Score
-            match_result = all_matches[i].find("div",{'class':'MResult'}).find_all('span',{'class':'score'})
+            match_result = (
+                all_matches[i]
+                .find("div", {"class": "MResult"})
+                .find_all("span", {"class": "score"})
+            )
             score = f"({match_result[0].text.strip()} - {match_result[1].text.strip()})"
-            
-            # Ger Match time 
-            match_time = all_matches[i].find('div',{'class':'MResult'}).find('span','time').text.strip()
-            
+
+            # Ger Match time
+            match_time = (
+                all_matches[i]
+                .find("div", {"class": "MResult"})
+                .find("span", "time")
+                .text.strip()
+            )
+
             # print(teamA)
             # Add Matches Info to matches_details
-            matches_details.append({
-                'Champion Type':championShip_title,
-                'First Team':teamB,
-                'Second Team':teamA,
-                'Match Time':match_time,
-                'RESULT':score,
-                })
-            
-    
-    for i in range(len(championShips)):
-        get_match_info(championShips[i])    
-    
+            matches_details.append(
+                {
+                    "Champion Type": champion_ship_title,
+                    "First Team": team_b,
+                    "Second Team": team_a,
+                    "Match Time": match_time,
+                    "RESULT": score,
+                }
+            )
+
+    # for i in range(len(champion_ships)):
+    #     get_match_info(champion_ships[i])
+
+    # Using
+    for champion_ship in champion_ships:
+        get_match_info(champion_ship)
+
+
+
     # Save to CSV File
-    keys=matches_details[0].keys()
-    
-    with open(f'yallaCora/champion_details.csv', 'w' , encoding='utf-8-sig', newline='') as output_file:
-        dict_writer   = csv.DictWriter(output_file, keys) 
+    keys = matches_details[0].keys()
+
+    with open(
+        "yallaCora/champion_details.csv", "w", encoding="utf-8-sig", newline=""
+    ) as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(matches_details)
         print(" file Created: ")
-        
-    
-    
+
     # "C:\Users\Mohamed\Desktop"
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     main(page)
